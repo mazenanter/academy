f1cdfaa797d429c7c9f82ea5a820fbf0b1cc37e3
Khalid Abdelrazk <khalidabdelrazk4@gmail.com>
2025-09-20 18:05:14 +0300

    Full Logic integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:academy/academy_app.dart';
+import 'package:academy/core/di/di.dart';
 import 'package:academy/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -6,5 +7,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  configureDependencies();
   runApp(AcademyApp(appRouter: AppRouter()));
 }
