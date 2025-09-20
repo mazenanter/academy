a4add1f2b0d85c62f18612f065b8cdafefa0ca9c
Khalid Abdelrazk <khalidabdelrazk4@gmail.com>
2025-09-20 18:04:00 +0300

    Add register repository interface and use case

--- lib/features/authentication/domain/repository/register_repository.dart
+++ lib/features/authentication/domain/repository/register_repository.dart
@@ -1,6 +1,6 @@
 import 'package:academy/core/failures/failures.dart';
-import 'package:academy/features/authentication/domain/Entity/register_response_entity.dart';
 import 'package:academy/features/authentication/domain/entity/register_request_body.dart';
+import 'package:academy/features/authentication/domain/entity/register_response_entity.dart';
 import 'package:either_dart/either.dart';
 
 abstract class RegisterRepository {
--- lib/features/authentication/domain/use case/register_use_case.dart
+++ lib/features/authentication/domain/use case/register_use_case.dart
@@ -1,10 +1,11 @@
 import 'package:academy/core/failures/failures.dart';
-import 'package:academy/features/authentication/domain/Entity/register_response_entity.dart';
 import 'package:academy/features/authentication/domain/entity/register_request_body.dart';
+import 'package:academy/features/authentication/domain/entity/register_response_entity.dart';
 import 'package:academy/features/authentication/domain/repository/register_repository.dart';
 import 'package:either_dart/either.dart';
+import 'package:injectable/injectable.dart';
 
-// @injectable
+@injectable
 class RegisterUseCase {
   final RegisterRepository registerRepository;
   RegisterUseCase({required this.registerRepository});
